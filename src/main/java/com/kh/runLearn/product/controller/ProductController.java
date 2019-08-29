d1fd6e2dd56a60874ee64bd6a5981c6ee2c91e56
SOOMIN KIM <tnalsdhksrms@naver.com>
2019-08-29 17:37:53 +0900

    rebase
    rebase

--- src/main/java/com/kh/runLearn/product/controller/ProductController.java
+++ src/main/java/com/kh/runLearn/product/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.kh.runLearn.product.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.runLearn.product.model.service.ProductService;
 
@@ -9,6 +12,14 @@ import com.kh.runLearn.product.model.service.ProductService;
 public class ProductController {
 
 	@Autowired
-	private ProductService pService;
+	private ProductService sService;
 
+	@RequestMapping("psearchAll.do")
+	public ModelAndView productSearchAll(ModelAndView mv, @RequestParam("search") String search) {
+		mv.addObject("search", search);
+		mv.addObject("cate", "상품");
+		mv.setViewName("search/searchDetailView");
+		
+		return mv;
+	}
 }
