823baec1ede3429a400045ca8a1582c0a747731d
kaleidoscopelim <46822711+kaleidoscopelim@users.noreply.github.com>
2019-09-18 17:34:00 +0900

    0918
    롤백함

--- src/main/java/com/kh/runLearn/admin/model/dao/AdminDAO.java
+++ src/main/java/com/kh/runLearn/admin/model/dao/AdminDAO.java
@@ -43,92 +43,13 @@ public class AdminDAO {
 	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
 		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
 	}
-	public ArrayList<Member> allUserListId(PageInfo pi) {//아이디 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserListId", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListNick(PageInfo pi) {//닉네임 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListEm(PageInfo pi) {//이메일 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListPh(PageInfo pi) {//폰 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListMD(PageInfo pi) {//수정일 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListG(PageInfo pi) {//등급 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListS(PageInfo pi) {//상태 유저 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
 	
-	//오름
-	public ArrayList<Member> allUserListR(PageInfo pi) {//모든 유저 정보 가지고 오기
+	
+	
+	public ArrayList<Member> searchId(PageInfo pi) {//이이디 검색 정보 가지고 오기
 		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
 	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListIdR(PageInfo pi) {//아이디 유저 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListNickR(PageInfo pi) {//닉네임 유저 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListEmR(PageInfo pi) {//이메일 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListPhR(PageInfo pi) {//폰 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListMDR(PageInfo pi) {//수정일 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListGR(PageInfo pi) {//등급 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListSR(PageInfo pi) {//상태 정보 가지고 오기
-		 int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserList", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListName(PageInfo pi) {
-		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserListName", null, rowBounds); // arraylist로 형변환해주기
-	}
-	public ArrayList<Member> allUserListNameR(PageInfo pi) {
-		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); // 계산법 현재페이지가 1이면 0
-	      RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
-		return (ArrayList)sqlSession.selectList("adminMapper.allUserListNameR", null, rowBounds); // arraylist로 형변환해주기
+		return (ArrayList)sqlSession.selectList("adminMapper.searchId", null, rowBounds); // arraylist로 형변환해주기
 	}
 	
 	
@@ -136,7 +57,7 @@ public class AdminDAO {
 	
 	
 	
-	public int targetUserUpdate(Member m) {//유저 회원 정보 가져오
+	public int targetUserUpdate(Member m) {//유저 회원 정보 수정
 		System.out.println("dao"+m);
 		return sqlSession.update("adminMapper.targetUserUpdate",m);
 	}
